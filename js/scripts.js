ae718cf8febabd426996b9576dbfc8f09717c0ec
Philipp <PhilSantay@gmail.com>
2017-04-04 11:58:53 +0200

    wip

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+
+  const hamburger = document.querySelector('.menu');
+  const sub = document.querySelector('.submenu');
+
+  /*
+   * Submenu Toggle
+   */
+  hamburger.addEventListener('click', (e) => {
+    e.preventDefault();
+    sub.classList.toggle('submenu-active');
+  })
+
+/**
+ * Toggle Buttons
+ */
+
+  $('.button').click(function(){
+    $(this).toggleClass('active');
+  })
+
+
+})
